cf4ce700e496619b0a3bc17c9c255977ad56dfd8
GavDotZip <gavdotzip@gmail.com>
2024-02-08 00:21:12 +0000

    comment commit

--- src/xlsxCreator.py
+++ src/xlsxCreator.py
@@ -10,18 +10,22 @@ TITLE = ['CEO', 'CFO', 'CTO']
 # Generate Rows of Pre-Defined Data
 rows = []
 for i in range(1, 4):
-    # print(i)
+    # Select a random name, location, and job title
     name = random.choice(NAME)
     location = random.choice(LOCATION)
     title = random.choice(TITLE)
+    # Append the data as a row
     rows.append([name, location, title])
 
-# Create, Fill and Save Excel
+# Create, Fill, and Save Excel
 wb = Workbook()
 ws = wb.active
+# Append the header row
 ws.append(HEADER)
 
+# Append each row of data
 for row in rows:
     ws.append(row)
 
+# Save the Excel file
 wb.save('tester.xlsx')
