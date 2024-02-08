6da25f4f403f8006c3b893b467bdc4f1e0cdebef
GavDotZip <gavdotzip@gmail.com>
2024-02-08 00:10:01 +0000

    define commit

--- src/xlsxCreator.py
+++ src/xlsxCreator.py
@@ -1,8 +1,11 @@
-# Imports
-
+import random
+import openpyxl
 
 # Define Headers and Data to be Inputted
-
+HEADER = ['Name', 'Location', 'Job Title']
+NAME = ['Mark Corrigan', 'Alan Johnson', 'Sophie Chapman']
+LOCATION = ['London', 'Aberdeen', 'Frankfurt']
+TITLE = ['CEO', 'CFO', 'CTO']
 
 # Generate Rows of Pre-Defined Data
 
